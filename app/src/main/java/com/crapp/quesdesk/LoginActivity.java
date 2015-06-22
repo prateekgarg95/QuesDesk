167d35f736a2ae81afee49e3307c0086f9ccd322
Prateek Garg <prateekgarg95@gmail.com>
2015-06-22 11:23:16 +0530

    Initial commit

--- app/src/main/java/com/crapp/quesdesk/LoginActivity.java
+++ app/src/main/java/com/crapp/quesdesk/LoginActivity.java
@@ -3,7 +3,7 @@ package com.crapp.quesdesk;
 // Import Statements
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
@@ -48,11 +48,13 @@ public class LoginActivity extends Activity implements
     // Google Sign In Button
     SignInButton signInButton;
 
-    //
+    // UI for showing background process
+    ProgressDialog progressDialog;
 
 
     // Server URL where user details needs to be stored
     private String userDetailsURL;
+
     // JSONObject Response from the Server
     private JSONObject jsonObject;
 
@@ -64,14 +66,17 @@ public class LoginActivity extends Activity implements
 
     // If the SignIn Button is clicked
     private boolean mSignInClicked;
+
     // Connection Result instance
     private ConnectionResult mConnectionResult;
+
     // TransferDataHTTP instance
     private TransferDataHTTP transferDataHTTP;
 
     // Method called when the Activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -95,6 +100,7 @@ public class LoginActivity extends Activity implements
     // Method called when the Activity is started
     protected void onStart() {
         super.onStart();
+
         // Connect the Google Client
         mGoogleApiClient.connect();
     }
@@ -102,6 +108,7 @@ public class LoginActivity extends Activity implements
     // Method called when the Activity is stopped
     protected void onStop() {
         super.onStop();
+
         // Check if the Google Client is connected
         if (mGoogleApiClient.isConnected()) {   // If the Google Client is connected then disconnect
             mGoogleApiClient.disconnect();
@@ -113,17 +120,23 @@ public class LoginActivity extends Activity implements
      * Called if there is any Sign In error
      */
     private void resolveSignInError() {
+
         // Check if the ConnectionResult Error has any resolution
         if (mConnectionResult.hasResolution()) {
+
             try {
                 // Set IntentInProgress as true
                 mIntentInProgress = true;
+
                 // Start Resolution for the result
                 mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+
             } // Catch Exception
             catch (IntentSender.SendIntentException e) {
+
                 // Set IntentInProgress as false
                 mIntentInProgress = false;
+
                 // Retry to connect Google Client
                 mGoogleApiClient.connect();
             }
@@ -133,14 +146,18 @@ public class LoginActivity extends Activity implements
     // Method called when the connection failed
     @Override
     public void onConnectionFailed(ConnectionResult result) {
+
         // If the ConnectionResult has no resolution
         if (!result.hasResolution()) {
+
             // Show Error dialog
             GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+
             return;
         }
 
         if (!mIntentInProgress) {
+
             // Store the ConnectionResult for later usage
             mConnectionResult = result;
 
@@ -172,55 +189,111 @@ public class LoginActivity extends Activity implements
     // Method called when Google Client is connected
     @Override
     public void onConnected(Bundle arg0) {
+
         // Set if SignIn Button clicked to false
         mSignInClicked = false;
 
+        // Setting Progress Dialog
+        progressDialog = new ProgressDialog(getBaseContext(), ProgressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("User Registration");
+        progressDialog.setMessage("Fetching User Data");
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
 
         // Get user's information
         getProfileInformation();
+
+        // Changing the message in ProgressDialog
+        progressDialog.setMessage("Setting the parameters");
+
+        // Setting the User Detail Parameters
         Map<String, String> params = new HashMap<>();
         params.put("Name", name);
         params.put("Email", email);
+
+        // Changing the message in ProgressDialog
+        progressDialog.setMessage("Connecting the Server");
+
+        // Received JSONObject
         jsonObject = transferDataHTTP.sendParams(userDetailsURL, params);
-        boolean success;
+
+        // Declaring the successMessage
+        String successMessage;
+
         try {
-            success = (boolean) jsonObject.get("success");
+            // Retrieving the successMessage
+            successMessage = jsonObject.getString("success");
         } catch (JSONException e) {
             e.printStackTrace();
-            success = false;
+            successMessage = null;
         }
-        if (success) {
+
+        // Checking if Registration was successful
+        if (Integer.parseInt(successMessage) == 1) {
+
+            // Set successfully logged in
             successfulLogin = true;
+
+            // Creating User Data SharedPreferences
             SharedPreferences userPreferences = getSharedPreferences("USER", MODE_PRIVATE);
+
+            // Inserting the User Data (i.e. Name and Email)
             SharedPreferences.Editor editor = userPreferences.edit();
             editor.putString("Name", name);
             editor.putString("Email", email);
             editor.putBoolean("LoginSuccess", successfulLogin);
             editor.commit();
+
+            // Sign Out from Google
             signOutFromGplus();
+
+            // Show Dashboard Screen
             Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
             startActivity(intent);
+
+            // Close the ProgressDialog
+            progressDialog.dismiss();
+
+            // Delete the Login Screen from Activity History
             finish();
+
+            // Show that the User is connected
             Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
         } else {
+
+            // LogIn Unsuccessful
             successfulLogin = false;
+
+            // Sign Out from Google
             signOutFromGplus();
+
+            // Close the ProgressDialog
+            progressDialog.dismiss();
+
+            // Show that a Server Error occurred
             Toast.makeText(this, "Server Error!", Toast.LENGTH_LONG).show();
         }
     }
 
+    // Method to obtain Profile information of the User
     private void getProfileInformation() {
+
         try {
+            // Check for current Profile
             if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
-                Person currentPerson = Plus.PeopleApi
-                        .getCurrentPerson(mGoogleApiClient);
+
+                // Get the current user details
+                Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
                 String personName = currentPerson.getDisplayName();
                 String personGooglePlusProfile = currentPerson.getUrl();
                 String personEmail = Plus.AccountApi.getAccountName(mGoogleApiClient);
 
+                // Adding Info to LogCat
                 Log.e(TAG, "Name: " + personName + ", plusProfile: "
                         + personGooglePlusProfile + ", email: " + personEmail);
 
+                // Storing User Data
                 name = personName;
                 email = personEmail;
             } else {
