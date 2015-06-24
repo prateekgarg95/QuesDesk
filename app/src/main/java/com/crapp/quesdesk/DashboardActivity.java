dfa8e9218ea3da6da136c173c0afd0259fc36504
Prateek Garg <prateekgarg95@gmail.com>
2015-06-24 19:38:20 +0530

    Initial commit

--- app/src/main/java/com/crapp/quesdesk/DashboardActivity.java
+++ app/src/main/java/com/crapp/quesdesk/DashboardActivity.java
@@ -1,30 +1,19 @@
 package com.crapp.quesdesk;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 
-public class DashboardActivity extends Activity {
+public class DashboardActivity extends ListActivity {
 
-    TextView txtName, txtEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        txtName = (TextView) findViewById(R.id.name);
-        txtEmail = (TextView) findViewById(R.id.email);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("USER",MODE_PRIVATE);
-        txtName.setText(sharedPreferences.getString("Name",null));
-        txtEmail.setText(sharedPreferences.getString("Email",null));
-
 
 
     }
